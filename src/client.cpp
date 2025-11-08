8e98328d2c73772d34382b3db3e05662b211f48e
myanwc <anton.kostyrev@yandex.ru>
2025-11-08 16:29:29 +0300

    Remove ReportError()

--- src/client.cpp
+++ src/client.cpp
@@ -2,10 +2,6 @@
 
 namespace irc {
 
-    void ReportError(sys::error_code ec, std::string_view where) {
-        std::string message = fmt::format("Error in {}: {} (code: {})",
-            where, ec.message(), ec.value());
-        LOG_ERROR(message);
-    }
+
 
 } // namespace irc
