24aac6ccae43a87ed4a77998687bea5cd9ef4ac1
myanwc <anton.kostyrev@yandex.ru>
2025-11-08 17:19:05 +0300

    just for threads debug

--- src/message_handler.h
+++ src/message_handler.h
@@ -17,12 +17,12 @@ namespace irc {
 
         class MessageHandler {
         public:
-            template <typename Owner>
-            void operator()(const std::vector<domain::Message>& messages, std::shared_ptr<Owner> owner) {
+            void operator()(const std::vector<domain::Message>& messages) {
                 for (const auto& message : messages) {
                     try {
                         if (message.GetMessageType() == irc::domain::MessageType::PRIVMSG) {
                             LOG_DEBUG(std::string(message.GetNick()).append(": "s).append(message.GetContent()));
+                            //std::osyncstream(std::cout) << "Message from " << message.GetNick() << " handled in thread: " << std::this_thread::get_id() << "\n";
                         }
                         else if (message.GetMessageType() == irc::domain::MessageType::UNKNOWN) {
                             std::ofstream fuckedup("FUCKED_UP.txt", std::ios::app);
@@ -32,12 +32,11 @@ namespace irc {
                     }
                     catch (const std::exception& e) {
                         std::ofstream fuckedup("FUCKED_UP.txt", std::ios::app);
-                        fuckedup << "[Thats fucked up] " << "\nReason: " << e.what() 
+                        fuckedup << "[Thats fucked up] " << "\nReason: " << e.what()
                             << "\n|" << message.GetRawPart() << "\n[End of Fucked up]" << std::endl;
                         LOG_FUCKUP("FUCK. But its logged");
                     }
                 }
-                owner->Read();
             }
 
         };
