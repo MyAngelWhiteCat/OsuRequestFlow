d1ca1511dab9a2576138abb6b296628b525c46b4
myanwc <anton.kostyrev@yandex.ru>
2025-11-10 20:26:12 +0300

    Add connection_mutex

--- src/message_handler.h
+++ src/message_handler.h
@@ -18,14 +18,16 @@ namespace irc {
 
         class MessageHandler {
         public:
-            MessageHandler(std::shared_ptr<connection::Connection> connection)
+            MessageHandler(std::shared_ptr<connection::Connection> connection, std::mutex& connection_mutex)
                 : connection_(connection)
+                , connection_mutex_(connection_mutex)
             {
             }
 
             void operator()(const std::vector<domain::Message>& messages);
 
         private:
+            std::mutex& connection_mutex_;
             std::shared_ptr<connection::Connection> connection_;
 
             void SendPong(const std::string_view content);
