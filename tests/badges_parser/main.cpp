b8e1f4a34a71ed34cc1e4d2eca191bd76e966fd4
myanwc <anton.kostyrev@yandex.ru>
2025-11-10 21:28:58 +0300

    Add infrasturcure

---
+++ tests/badges_parser/main.cpp
@@ -0,0 +1,5 @@
+#include "test_badges_parser.h"
+
+int main() {
+    test_badge_parser::RunTests();
+}
--- tests/badges_parser/test_badges_parser.h
+++ tests/badges_parser/test_badges_parser.h
@@ -1,35 +1,22 @@
+#include <string>
+
 #include "../../src/message.h"
+#include "../../src/domain.h"
 
 namespace test_badge_parser {
 
     void TestParsingPRIVMSG() {
-        std::string PRIVMSG = "@badge-info=;"
-            "badges=broadcaster/1;"
-            "client-nonce=459e3142897c7a22b7d275178f2259e0;"
-            "color=#0000FF;"
-            "display-name=USERNAME;"
-            "emote-only=1;"
-            "emotes=62835:0-10;"
-            "first-msg=0;"
-            "flags=;"
-            "id=885196de-cb67-427a-baa8-82f9b0fcd05f;"
-            "mod=0;"
-            "room-id=713936733;"
-            "subscriber=0;"
-            "tmi-sent-ts=1643904084794;"
-            "turbo=0;"
-            "user-id=713936733;"
-            "user-type="
-            ":lovingt3s!lovingt3s@lovingt3s.tmi.twitch.tv"
-            "PRIVMSG"
-            "CHANNEL"
-            "MESSAGE"s;
-
+        std::string PRIVMSG = "@badge-info=;badges=broadcaster/1;client-nonce=459e3142897c7a22b7d275178f2259e0;color=#0000FF;display-name=lovingt3s;emote-only=1;emotes=62835:0-10;first-msg=0;flags=;id=885196de-cb67-427a-baa8-82f9b0fcd05f;mod=0;room-id=713936733;subscriber=0;tmi-sent-ts=1643904084794;turbo=0;user-id=713936733;user-type= :lovingt3s!lovingt3s@lovingt3s.tmi.twitch.tv PRIVMSG #lovingt3s :bleedPurple";
         
+        auto split = irc::domain::Split(PRIVMSG);
+        std::cout << split.size() << std::endl;
+        irc::domain::Message msg(irc::domain::MessageType::PRIVMSG, std::string(split[4]), std::string(split[0]));
+        auto badges = msg.GetBadges();
+        std::cout << badges.at("color")[0].substr(1) << std::endl;
     }
 
     void RunTests() {
-
+        TestParsingPRIVMSG();
     }
 
 }
