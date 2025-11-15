71c56b93ccd8351a637d68db6d8eb7c92ada9006
myanwc <anton.kostyrev@yandex.ru>
2025-11-15 21:50:05 +0300

    Edit logic of download. Class is preaty ready to test. Need to find out is it important to write osz file name like a map name

--- src/downloader.h
+++ src/downloader.h
@@ -3,6 +3,9 @@
 #include <memory>
 #include <string>
 #include <string_view>
+#include <vector>
+#include <utility>
+#include <unordered_map>
 
 #include "file_manager.h"
 #include "random_user_agent.h"
@@ -12,6 +15,7 @@
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/strand.hpp>
 #include <boost/asio/ssl/context.hpp>
+#include <stdexcept>
 
 namespace downloader {
 
@@ -22,31 +26,143 @@ namespace downloader {
     using namespace std::literals;
 
     using Strand = net::strand<net::io_context::executor_type>;
+    using ResoursesAccess = std::unordered_map<std::string, std::shared_ptr<http_domain::Client>>;
 
     class Downloader : public std::enable_shared_from_this<Downloader> {
 
     public:
-        Downloader(net::io_context& ioc)
-            : user_agent_(100)
-            , 
+
+        Downloader(net::io_context& ioc
+            , std::shared_ptr<ssl::context> ctx
+            , std::vector<std::string> resourses
+            , std::string_view user_agent
+            , file_manager::FileManager file_manager)
+            : ioc_(ioc)
+            , ctx_(ctx)
+            , resourses_(resourses)
+            , user_agent_(user_agent)
+            , file_manager_(file_manager)
         {
-            
+            CheckResoursesForEmpty();
+            SetupFirstConnection();
         }
 
-        Downloader(net::io_context& ioc, std::shared_ptr<ssl::context> ctx)
-            : user_agent_(100)
-            , 
-        {
+        Downloader(net::io_context& ioc
+            , std::vector<std::string> resourses
+            , std::string_view user_agent
+            , file_manager::FileManager file_manager)
+            : ioc_(ioc)
+            , resourses_(resourses)
+            , user_agent_(user_agent)
+            , file_manager_(file_manager)
 
+        {
+            CheckResoursesForEmpty();
+            SetupFirstConnection();
         }
 
-        void Download(std::string_view host, std::string_view port, bool secured = true) {
-            
+        Downloader(net::io_context& ioc
+            , std::shared_ptr<ssl::context> ctx
+            , std::vector<std::string> resourses
+            , RandomUserAgent user_agent
+            , file_manager::FileManager file_manager)
+            : ioc_(ioc)
+            , ctx_(ctx)
+            , resourses_(resourses)
+            , user_agent_changer_(std::make_unique<RandomUserAgent>(user_agent))
+            , file_manager_(file_manager)
+        {
+            CheckResoursesForEmpty();
+            SetupFirstConnection();
+        }
+
+        Downloader(net::io_context& ioc
+            , std::vector<std::string> resourses
+            , RandomUserAgent user_agent
+            , file_manager::FileManager file_manager)
+            : ioc_(ioc)
+            , resourses_(resourses)
+            , user_agent_changer_(std::make_unique<RandomUserAgent>(user_agent))
+            , file_manager_(file_manager)
+        {
+            CheckResoursesForEmpty();
+            SetupFirstConnection();
+        }
+
+        void Download(std::string_view uri) {
+            if (user_agent_changer_) {
+                user_agent_ = user_agent_changer_->GetUserAgent();
+            }
+            for (const auto& [resourse, client] : resourse_to_client_) {
+                if (client->IsConnected()) {
+                    client->Get(uri, user_agent_, [self = shared_from_this()]
+                    (std::unordered_map<std::string, std::string>&& headers_to_value, std::string&& body) {
+                            self->OnDownload(std::move(headers_to_value), std::move(body));
+                        });
+                }
+            }
         }
 
     private:
-        RandomUserAgent user_agent_;
-        http::Client http_client;
+        net::io_context& ioc_;
+        std::shared_ptr<ssl::context> ctx_{ nullptr };
+        std::string user_agent_;
+        std::unique_ptr<RandomUserAgent> user_agent_changer_{ nullptr };
+        std::vector<std::string> resourses_;
+        ResoursesAccess resourse_to_client_;
+        file_manager::FileManager file_manager_;
+        bool secured_ = true;
+
+        void OnDownload(std::unordered_map<std::string, std::string> headers_to_value, std::string body) {
+            // I am not sure about map name.. i need to find out does it have any sense...
+        }
+
+        void WriteOnDisk(std::string&& bytes, std::string&& file_name) {
+            net::post(ioc_, [self = this->shared_from_this()
+                , bytes = std::move(bytes), file_name = std::move(file_name)]() mutable {
+                self->file_manager_.WriteInRoot(std::move(bytes), std::move(file_name));
+                });
+        }
+
+        void SetupFirstConnection() {
+            std::shared_ptr<http_domain::Client> client;
+            if (ctx_) {
+                client = SetupSecuredConnection();
+            }
+            else {
+                client = SetupNonSecuredConnection();
+            }
+
+        }
+
+        std::shared_ptr<http_domain::Client> SetupConnection(std::shared_ptr<http_domain::Client> client
+            , std::string_view port) {
+            for (const auto& resourse : resourses_) {
+                client->Connect(resourse, port);
+                if (client->IsConnected()) {
+                    return client;
+                }
+            }
+            throw std::runtime_error("All resourses unavailable");
+        }
+
+        std::shared_ptr<http_domain::Client> SetupNonSecuredConnection() {
+            auto client = std::make_shared<http_domain::Client>(ioc_);
+            return SetupConnection(client, http_domain::Port::NON_SECURED);
+
+        }
+
+        std::shared_ptr<http_domain::Client> SetupSecuredConnection() {
+            auto client = std::make_shared<http_domain::Client>(ioc_);
+            return SetupConnection(client, http_domain::Port::SECURED);
+        }
+
+        void CheckResoursesForEmpty() {
+            if (resourses_.empty()) {
+                throw std::runtime_error("Empty resourses");
+            }
+        }
+
     };
 
 }
