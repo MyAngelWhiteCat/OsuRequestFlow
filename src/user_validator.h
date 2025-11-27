2d31c19483cdc664e7d9c9bafb9f2d07ea9b6687
myanwc <anton.kostyrev@yandex.ru>
2025-11-27 17:18:51 +0300

    Delete serialization logic

--- src/user_validator.h
+++ src/user_validator.h
@@ -50,22 +50,23 @@ namespace commands {
             }
 
         private:
-            irc::domain::Role accept_from_;
-        };
-
-        struct DomainNames {
-            static constexpr std::string_view SETTINGS = "settings.cfg"sv;
-            static constexpr std::string_view DELIMER = "-END-OF-LIST-"sv;
+            irc::domain::Role accept_from_ = irc::domain::Role::MODERATOR;
         };
 
         class UserVerificator {
         public:
             UserVerificator()
-                : file_manager_(std::filesystem::current_path() / "users"s)
-                , black_list_(std::make_unique<std::unordered_set<std::string>>())
+                : black_list_(std::make_unique<std::unordered_set<std::string>>())
                 , white_list_(std::make_unique<std::unordered_set<std::string>>())
             {
-                LoadSettings();
+            }
+
+            UserVerificator(std::vector<std::string>& white_list, std::vector<std::string>& black_list)
+                : black_list_(std::make_unique<std::unordered_set<std::string>>
+                    (black_list.begin(), black_list.end()))
+                , white_list_(std::make_unique<std::unordered_set<std::string>>
+                    (white_list.begin(), white_list.end()))
+            {
             }
 
             bool Verify(const std::string_view user_name, const irc::domain::Role& role) {
@@ -108,6 +109,10 @@ namespace commands {
                 return role_filter_.GetLevel();
             }
 
+            bool GetWhiteListOnly() const {
+                return whitelist_only_;
+            }
+
             std::unordered_set<std::string>* GetWhiteList() {
                 return white_list_.get();
             }
@@ -116,72 +121,12 @@ namespace commands {
                 return black_list_.get();
             }
 
-            void SaveSettings() {
-                std::vector<char> serialized;
-                serialized.push_back(static_cast<char>(whitelist_only_));
-                serialized.push_back('\n');
-                serialized.push_back(role_filter_.GetLevel());
-                serialized.push_back('\n');
-                SerializeListTo(serialized, *white_list_);
-                DelimSerialized(serialized, std::string(DomainNames::DELIMER));
-                SerializeListTo(serialized, *black_list_);
-                file_manager_.WriteCharsInRoot(std::string(DomainNames::SETTINGS), std::move(serialized), false);
-            }
-
-            void LoadSettings() {
-                auto serialized = file_manager_.ReadCharsFromRoot(DomainNames::SETTINGS);
-                if (!serialized || serialized->empty()) {
-                    SaveSettings();
-                    return;
-                }
-                whitelist_only_ = static_cast<bool>((*serialized)[0]);
-                role_filter_.SetLevel((*serialized)[2]); // number 0 - 4;
-                size_t start_of_white_list = 3;
-                size_t start_of_black_list = LoadList(*serialized, white_list_.get(), start_of_white_list);
-                LoadList(*serialized, black_list_.get(), start_of_black_list);
-            }
-
         private:
-            file_manager::FileManager file_manager_;
             RoleFilter role_filter_;
             std::unique_ptr<std::unordered_set<std::string>> black_list_;
             std::unique_ptr<std::unordered_set<std::string>> white_list_;
             bool whitelist_only_ = false;
 
-            void SerializeListTo(std::vector<char>& input, std::unordered_set<std::string> list) {
-                for (const auto& elem : list) {
-                    for (const auto& ch : elem) {
-                        input.push_back(ch);
-                    }
-                    input.push_back('\n');
-                }
-            }
-
-            void DelimSerialized(std::vector<char>& input, std::string delimer) {
-                for (const auto& ch : delimer) {
-                    input.push_back(ch);
-                }
-                input.push_back('\n');
-            }
-
-            size_t LoadList(std::vector<char> chars, std::unordered_set<std::string>* list, size_t from) {
-                std::string name;
-                for (from; from < chars.size(); ++from) {
-                    char& ch = chars[from];
-                    if (ch == '\n') {
-                        if (name == DomainNames::DELIMER) {
-                            return ++from;
-                        }
-                        LOG_INFO(name);
-                        list->insert(name);
-                        name.clear();
-                    }
-                    else {
-                        name += ch;
-                    }
-                }
-                return 0;
-            }
         };
 
     }
