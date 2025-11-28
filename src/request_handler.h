32546984cbb7de57981bb5488c3b44130557a09d
myanwc <anton.kostyrev@yandex.ru>
2025-11-28 22:06:14 +0300

    Setup basic handling

--- src/request_handler.h
+++ src/request_handler.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include "core.h"
+#include "api_request_handler.h"
+#include "file_request_handler.h"
 
-#include <memory>
 
 namespace asio = boost::asio;
 namespace beast = boost::beast;
@@ -12,13 +13,29 @@ namespace gui_http {
 
     class RequestHandler {
     public:
-        template <typename req, typename send>
-        void operator()(req&&, send&&) {
-            
+        RequestHandler(core::Core& core, const std::filesystem::path& root)
+            : core_(core)
+            , file_handler_(root)
+        {
+
+        }
+
+        template <typename Body, typename Allocator, typename Send>
+        void operator()(http::request<Body, http::basic_fields<Allocator>>&& req, Send&& send) {
+            std::string_view target = req.target();
+            if (target.substr(0, api_prefix_.size()) == api_prefix_) {
+                api_handler_(std::move(req), std::move(send)); // NEED TO DISPATCH IN API STRAND!!!
+            }
+            else {
+                file_handler_(std::move(req), std::move(send));
+            }
         }
 
     private:
-        std::unique_ptr<core::Core> core_;
+        core::Core& core_;
+        FileRequestHandler file_handler_;
+        ApiRequestHandler api_handler_{ core_ };
+        const std::string api_prefix_ = "/api/";
 
     };
 
