3e48bcc793c29d67f02766ecc2ceb598ca536d2f
myanwc <anton.kostyrev@yandex.ru>
2025-11-29 22:04:35 +0300

    Add validators for all endpoints

--- src/request_validator.h
+++ src/request_validator.h
@@ -10,10 +10,10 @@
 
 namespace request_validator {
 
-    
+
     namespace beast = boost::beast;
     namespace http = beast::http;
-    
+
     using json = nlohmann::json;
 
     class RequestValidator {
@@ -28,18 +28,39 @@ namespace request_validator {
 
         template <typename Body, typename Allocator, typename Send>
         std::optional<size_t> ValidateFileSizeRequest(http::request<Body, http::basic_fields<Allocator>>& req, Send&& send);
-        
-        template <typename Body, typename Allocator, typename Send>
-        std::optional<std::string_view> ValidateDownloadsFolderRequest(http::request<Body, http::basic_fields<Allocator>>& req, Send&& send);
 
-    private:
-        response_maker::ResponseMaker& response_maker_;
+        template <typename Body, typename Allocator, typename Send>
+        std::optional<std::string> ValidateDownloadsFolderRequest(http::request<Body, http::basic_fields<Allocator>>& req, Send&& send);
+
+        template <typename Body, typename Allocator, typename Send>
+        std::optional<std::string> ValidateListEditRequest(http::request<Body, http::basic_fields<Allocator>>& req, Send&& send);
+
+        template <typename Body, typename Allocator, typename Send>
+        std::optional<std::string> ValidateJoinRequest(http::request<Body, http::basic_fields<Allocator>>& req, Send&& send);
+
+        template <typename Body, typename Allocator, typename Send>
+        std::optional<std::string> ValidatePartRequest(http::request<Body, http::basic_fields<Allocator>>& req, Send&& send);
+
+        template <typename Body, typename Allocator, typename Send>
+        std::optional<std::pair<std::string, std::string>> ValidateSetResourseRequest(http::request<Body, http::basic_fields<Allocator>>& req, Send&& send);
+
+        template <typename Body, typename Allocator, typename Send>
+        std::optional<int> ValidateSetRoleFilterRequest(http::request<Body, http::basic_fields<Allocator>>& req, Send&& send);
+
+        template <typename Body, typename Allocator, typename Send>
+        std::optional<int> ValidateReconnectTimeoutRequest(http::request<Body, http::basic_fields<Allocator>>& req, Send&& send);
+
+        template <typename Body, typename Allocator, typename Send>
+        std::optional<bool> ValidateSetWhiteListOnlyRequest(http::request<Body, http::basic_fields<Allocator>>& req, Send&& send);
 
         template<typename Body, typename Allocator, typename Send>
         void SendMethodNotAllowed(http::request<Body, http::basic_fields<Allocator>>& req, Send&& send, std::string_view allowed);
 
         template<typename Body, typename Allocator, typename Send>
         void SendInvalidArgument(http::request<Body, http::basic_fields<Allocator>>& req, Send&& send, std::string_view info);
+
+    private:
+        response_maker::ResponseMaker& response_maker_;
 
         template<typename Body, typename Allocator, typename Send>
         std::optional<json> ParseJson(http::request<Body, http::basic_fields<Allocator>>& req, Send&& send);
@@ -60,7 +81,7 @@ namespace request_validator {
             SendMethodNotAllowed(req, send, "PUT");
             return std::nullopt;
         }
-        
+
         if (auto parsed_body = ParseJson(req, send)) {
             if (parsed_body->contains("FileSize") && (*parsed_body)["FileSize"].is_number_unsigned()) {
                 return (*parsed_body)["FileSize"].get<size_t>();
@@ -73,20 +94,118 @@ namespace request_validator {
     }
 
     template<typename Body, typename Allocator, typename Send>
-    inline std::optional<std::string_view> RequestValidator::ValidateDownloadsFolderRequest(http::request<Body, http::basic_fields<Allocator>>& req, Send&& send) {
+    inline std::optional<std::string> RequestValidator::ValidateDownloadsFolderRequest(http::request<Body, http::basic_fields<Allocator>>& req, Send&& send) {
         if (req.method() != http::verb::put) {
             SendMethodNotAllowed(req, send, "PUT");
             return std::nullopt;
         }
         if (auto parsed_body = ParseJson(req, send)) {
             if (parsed_body->contains("Path") && (*parsed_body)["Path"].is_string()) {
-                std::string_view path = (*parsed_body)["Path"].get<std::string_view>();
+                std::string path = (*parsed_body)["Path"].get<std::string>();
                 if (std::filesystem::exists(path)) {
                     return path;
                 }
+                SendInvalidArgument(req, send, "Path does not exist");
+                return std::nullopt;
+            }
+            SendInvalidArgument(req, send, "Invalid path");
+        }
+        return std::nullopt;
+    }
+
+    template<typename Body, typename Allocator, typename Send>
+    inline std::optional<std::string> RequestValidator::ValidateListEditRequest(http::request<Body, http::basic_fields<Allocator>>& req, Send&& send) {
+        if (auto parsed_body = ParseJson(req, send)) {
+            if (parsed_body->contains("UserName") && (*parsed_body)["UserName"].is_string()) {
+                return (*parsed_body)["UserName"].get<std::string>();
+            }
+            SendInvalidArgument(req, send, "Invalid UserName");
+        }
+        return std::nullopt;
+    }
+
+    template<typename Body, typename Allocator, typename Send>
+    inline std::optional<std::string> RequestValidator::ValidateJoinRequest(http::request<Body, http::basic_fields<Allocator>>& req, Send&& send) {
+        if (req.method() != http::verb::post) {
+            SendMethodNotAllowed(req, send, "POST");
+            return std::nullopt;
+        }
+        if (auto parsed_body = ParseJson(req, send)) {
+            if (parsed_body->contains("Channel") && (*parsed_body)["Channel"].is_string()) {
+                return (*parsed_body)["Channel"].get<std::string>();
+            }
+            SendInvalidArgument(req, send, "Invalid channel");
+        }
+        return std::nullopt;
+    }
+
+    template<typename Body, typename Allocator, typename Send>
+    inline std::optional<std::string> RequestValidator::ValidatePartRequest(http::request<Body, http::basic_fields<Allocator>>& req, Send&& send) {
+        return ValidateJoinRequest(req, send);
+    }
+
+    template<typename Body, typename Allocator, typename Send>
+    inline std::optional<std::pair<std::string, std::string>> RequestValidator::ValidateSetResourseRequest(http::request<Body, http::basic_fields<Allocator>>& req, Send&& send) {
+        if (req.method() != http::verb::put) {
+            SendMethodNotAllowed(req, send, "PUT");
+            return std::nullopt;
+        }
+        if (auto parsed_body = ParseJson(req, send)) {
+            if (parsed_body->contains("Resourse") && parsed_body->contains("Prefix")
+                && (*parsed_body)["Resourse"].is_string() && (*parsed_body)["Prefix"].is_string()) {
+                return std::make_pair((*parsed_body)["Resourse"].get<std::string>(), (*parsed_body)["Prefix"].get<std::string>());
+            }
+            SendInvalidArgument(req, send, "Invalid resourse or prefix");
+        }
+        return std::nullopt;
+    }
+
+    template<typename Body, typename Allocator, typename Send>
+    inline std::optional<int> RequestValidator::ValidateSetRoleFilterRequest(http::request<Body, http::basic_fields<Allocator>>& req, Send&& send) {
+        if (req.method() != http::verb::put) {
+            SendMethodNotAllowed(req, send, "PUT");
+            return std::nullopt;
+        }
+        json j;
+        if (auto parsed_body = ParseJson(req, send)) {
+            if (parsed_body->contains("RoleFilterLevel") && (*parsed_body)["RoleFilterLevel"].is_number()) {
+                int lvl = (*parsed_body)["RoleFilterLevel"].get<int>();
+                if (lvl >= 0 && lvl <= 4) {
+                    return lvl;
+                }
+                SendInvalidArgument(req, send, "Invalid level. Expected 0 - 4");
+                return std::nullopt;
+            }
+            SendInvalidArgument(req, send, "Invalid level");
+        }
+        return std::nullopt;
+    }
+
+    template<typename Body, typename Allocator, typename Send>
+    inline std::optional<int> RequestValidator::ValidateReconnectTimeoutRequest(http::request<Body, http::basic_fields<Allocator>>& req, Send&& send) {
+        if (req.method() != http::verb::put) {
+            SendMethodNotAllowed(req, send, "PUT");
+        }
+        if (auto parsed_body = ParseJson(req, send)) {
+            if (parsed_body->contains("ReconnectTimeout") && (*parsed_body)["ReconnectTimeout"].is_number_unsigned()) {
+                return (*parsed_body)["ReconnectTimeout"].get<unsigned>();
             }
         }
-        SendInvalidArgument(req, send, "Invalid path");
+        SendInvalidArgument(req, send, "Invalid time");
+        return std::nullopt;
+    }
+
+    template<typename Body, typename Allocator, typename Send>
+    inline std::optional<bool> RequestValidator::ValidateSetWhiteListOnlyRequest(http::request<Body, http::basic_fields<Allocator>>& req, Send&& send) {
+        if (req.method() != http::verb::put) {
+            SendMethodNotAllowed(req, send, "PUT");
+        }
+        if (auto parsed_body = ParseJson(req, send)) {
+            if (parsed_body->contains("IsOn") && (*parsed_body)["IsOn"].is_boolean()) {
+                return (*parsed_body)["IsOn"].get<bool>();
+            }
+        }
+        SendInvalidArgument(req, send, "Invalid status");
         return std::nullopt;
     }
 
@@ -114,8 +233,10 @@ namespace request_validator {
     template<typename Body, typename Allocator, typename Send>
     inline std::optional<json> RequestValidator::ParseJson(http::request<Body, http::basic_fields<Allocator>>& req, Send&& send) {
         json obj;
+
         try {
             obj = json::parse(req.body());
+            std::cout << obj.dump(4) << std::endl;
         }
         catch (const std::exception& e) {
             send(response_maker_.MakeStringResponse(
