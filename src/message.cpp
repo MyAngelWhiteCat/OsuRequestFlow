d5d2f2a7bb9d2e36fecffaf95fbcd70ba8b6a4ca
myanwc <anton.kostyrev@yandex.ru>
2025-11-29 22:02:58 +0300

    Fix badges parsing. Add Role

--- src/message.cpp
+++ src/message.cpp
@@ -5,6 +5,40 @@ namespace irc {
 
     namespace domain {
 
+        Message::Message(domain::MessageType message_type, std::string&& content, std::string&& badges)
+            : message_type_(message_type)
+            , content_(std::move(content))
+        {
+            if (badges.empty()) return;
+            std::string badge;
+            bool badge_seted = false;
+            std::string value;
+            for (const char ch : badges) {
+                if (!badge_seted && ch != '=') {
+                    badge += ch;
+                    continue;
+                }
+                else {
+                    badge_seted = true;
+                }
+                if (ch == ',') {
+                    badges_[badge].push_back(value);
+                    value.clear();
+                }
+                else if (ch == ';') {
+                    badges_[badge].push_back(value);
+                    value.clear();
+                    badge.clear();
+                    badge_seted = false;
+                }
+                else if (ch != '=') {
+                    value += ch;
+                }
+            }
+
+            SetRole();
+        }
+
         Message Message::TakeTypeAndMegre(Message&& other) {
             if (other.message_type_ == MessageType::PRIVMSG) {
                 for (auto& [badge, value] : other.badges_) {
@@ -49,9 +83,11 @@ namespace irc {
             return badges_;
         }
 
-        Role Message::GetMainBadge() const {
-            // TODO;
-            return Role::EMPTY;
+        Role Message::GetRole() const {
+            if (message_type_ != domain::MessageType::PRIVMSG) {
+                throw std::logic_error("Only PRIMSG can have badges");
+            }
+            return role_;
         }
 
         std::string Message::GetColorFromHex() const {
@@ -62,36 +98,49 @@ namespace irc {
             return "";
         }
 
-        Message::Message(domain::MessageType message_type, std::string&& content, std::string&& badges)
-            : message_type_(message_type)
-            , content_(std::move(content))
-        {
-            if (badges.empty()) return;
-            std::string badge;
-            bool badge_seted = false;
-            std::string value;
-            for (const char ch : badges) {
-                if (!badge_seted && ch != '=') {
-                    badge += ch;
-                    continue;
+        void Message::SetRole() {
+            if (auto it = badges_.find("badges"); it != badges_.end()) {
+                if (it->second.empty()) {
+                    role_ = Role::EMPTY;
+                    return;
                 }
-                else {
-                    badge_seted = true;
-                }
-                if (ch == ',') {
-                    badges_[badge].push_back(value);
-                    value.clear();
-                }
-                else if (ch == ';') {
-                    badges_[badge].push_back(value);
-                    value.clear();
-                    badge.clear();
-                    badge_seted = false;
-                }
-                else if (ch != '=') {
-                    value += ch;
+                auto& raw_badges = it->second;
+                for (const auto& raw_badge : raw_badges) {
+                    size_t delimer = raw_badge.find('/');
+                    if (delimer == std::string::npos) { 
+                        continue; 
+                    }
+                    
+                    std::string_view badge(raw_badge.data(), delimer);
+                    std::string_view value(raw_badge.data() + delimer + 1, raw_badge.size() - delimer - 1);
+
+                    std::cout << "badge.data(): " << (void*)badge.data() << std::endl;
+                    std::cout << "raw_badge.data(): " << (void*)raw_badge.data() << std::endl;
+
+                    std::cout << "RAW_BADGE: " << raw_badge << std::endl;
+                    std::cout << "Splited badge: " << badge << " value: " << value << "\n";
+                    if (value == "0") {
+                        continue;
+                    }
+                    if (badge == "broadcaster" && role_ < Role::BROADCASTER) {
+                        role_ = Role::BROADCASTER;
+                        return;
+                    }
+                    else if (badge == "moderator" && role_ < Role::MODERATOR) {
+                        role_ = Role::MODERATOR;
+                        return;
+                    }
+                    else if (badge == "vip" && role_ < Role::VIP) {
+                        role_ = Role::VIP;
+                        return;
+                    }
+                    else if (badge == "subscriber" && role_ < Role::SUBSCRIBER) {
+                        role_ = Role::SUBSCRIBER;
+                        return;
+                    }
                 }
             }
+            
         }
 
         bool Message::operator==(const Message& other) const {
