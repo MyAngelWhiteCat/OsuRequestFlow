c62a9a50abe615ce2bf40fa1b7357c6396611067
myanwc <anton.kostyrev@yandex.ru>
2025-11-30 23:48:09 +0300

    Its work. But its buggy

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,70 @@
 #include "auth_data.h"
 #include "irc_client.h"
 #include "logging.h"
-#include "connection.h"
 #include "downloader.h"
 #include "core.h"
+#include "http_server.h"
+#include "request_handler.h"
 
-
-#include <boost/asio/executor_work_guard.hpp>
 #include <boost/asio/impl/io_context.ipp>
 #include <boost/asio/io_context.hpp>
-#include <boost/asio/post.hpp>
-#include <boost/asio/strand.hpp>
 
 #include <clocale>
 
-#include <memory>
 #include <thread>
 #include <vector>
-#include <string_view>
-#include <exception>
 
 using namespace irc;
 namespace fs = std::filesystem;
 
+namespace net = boost::asio;
+namespace sys = boost::system;
+namespace ssl = net::ssl;
+using net::ip::tcp;
+using namespace std::literals;
+
+using Strand = net::strand<net::io_context::executor_type>;
+
+template <typename Fn>
+void RunWorkers(unsigned num_workers, const Fn& fn) {
+    std::vector<std::jthread> threads;
+    for (unsigned i = 0; i < num_workers - 1; ++i) {
+        threads.emplace_back(fn);
+    }
+    fn();
+}
 
 int main() {
     logging::Logger::Init();
     setlocale(LC_ALL, "Russian_Russia.1251");
 
     std::string resourse = "osu.direct";
-    std::string streamer = "myangelwhitecat";
     std::string uri_prefix = "/api/d/";
     std::string downloads_path = std::filesystem::current_path().string() + "/downloads";
 
-    core::Core core(2);
+    net::io_context ioc;
+
+    core::Core core(ioc);
     core.SetupConnection(true);
     core.SetupDownloader(true, resourse, uri_prefix, downloads_path);
     core.SetupChatBot();
     core.SetupIRCClient(true);
-    core.AddUserInBlackList("Nigger");
-    core.AddUserInWhiteList("myangelwhitecat");
-    core.SetRoleLevelFilter(2);
-    core.SetWhiteListOnly(true);
-    core.SaveSettings();
     core.Start();
 
-    core.Join(streamer);
-    core.Run(2);
+    auto address = net::ip::make_address("127.0.0.1");
+    boost::asio::ip::tcp::endpoint localhost{ address, 8181 };
+    std::filesystem::path root = std::filesystem::current_path() / "../static";
+
+    Strand api_strand = net::make_strand(ioc);
+    auto handler = std::make_shared<gui_http::RequestHandler>(core, root, api_strand);
+    http_server::ServeHttp(ioc, localhost, [handler](auto&& req, auto&& send) mutable {
+        (*handler)(std::move(req), std::move(send));
+        });
+
+    RunWorkers(6, [&ioc]() {
+        ioc.run();
+        });
+
     logging::Logger::Shutdown();
 }
 
