aab9707257a142e3a8680dfeeb7952c7664a7326
myanwc <anton.kostyrev@yandex.ru>
2025-12-05 21:43:03 +0300

    Edit starting

--- src/main.cpp
+++ src/main.cpp
@@ -37,8 +37,8 @@ void RunWorkers(unsigned num_workers, const Fn& fn) {
     fn();
 }
 
-bool OpenBrowserAtPort8181() {
-    std::string url = "http://127.0.0.1:8181";
+bool OpenBrowserAtPort23140() {
+    std::string url = "http://127.0.0.1:23140";
     HINSTANCE result = ShellExecuteA(
         nullptr,
         "open",
@@ -50,38 +50,39 @@ bool OpenBrowserAtPort8181() {
     return reinterpret_cast<int>(result) > 32;
 }
 
-
 int main() {
     logging::Logger::Init();
     setlocale(LC_ALL, "Russian_Russia.1251");
+
     net::io_context ioc;
+    LOG_INFO("Carefully look at this path ");
+    LOG_INFO(fs::current_path().string());
+    LOG_INFO("If this path have something like йцукенгшщзхъфывапролджячсмитьбю");
+    LOG_INFO("replace .exe to eng only folders path");
+    LOG_INFO("if path is OK Press ENTER.");
+
+    std::cin.get();
 
     core::Core core(ioc);
     core.SetupConnection(true);
     core.SetupDownloader(true);
     core.SetupIRCClient(true);
     core.Start();
+    core.LoadSettings();
 
     auto address = net::ip::make_address("127.0.0.1");
-    boost::asio::ip::tcp::endpoint localhost{ address, 8181 };
-    fs::path root = fs::current_path() / "static";
+    boost::asio::ip::tcp::endpoint localhost{ address, 23140 };
+    fs::path root = fs::current_path() / "../static";
 
-    LOG_INFO("MAKE SURE YOU DONT HAVE ANY kirilic/arabic/chineese NAMED FOLDERS IN PATH TO THIS EXE");
-    LOG_INFO("OR NOW YOU WILL SEE EPIC CRASH WITH 5+ ASSERTION");
-    LOG_INFO(fs::current_path().string());
-    LOG_INFO("IF THIS PATH DONT HAVE ANY ABRAKADABRA");
-    LOG_INFO("Fingerprint anything you want.");
-    std::string str;
-    std::cin >> str;
-
+    
     Strand api_strand = net::make_strand(ioc);
     auto handler = std::make_shared<gui_http::RequestHandler>(core, root, api_strand);
     http_server::ServeHttp(ioc, localhost, [handler](auto&& req, auto&& send) mutable {
         (*handler)(std::move(req), std::move(send));
         });
 
-    OpenBrowserAtPort8181();
-    LOG_INFO("GO TO http://localhost:8181");
+    OpenBrowserAtPort23140();
+    LOG_INFO("GO TO http://localhost:23140");
     RunWorkers(6, [&ioc]() {
         ioc.run();
         });
