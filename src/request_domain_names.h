0d967f0136c931e991d43fe9115eefb8b3fa5060
myanwc <anton.kostyrev@yandex.ru>
2025-12-06 20:31:58 +0300

    Add new file for all domain gui names

---
+++ src/request_domain_names.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <string>
+#include <string_view>
+
+namespace gui_http {
+
+    using namespace std::literals;
+
+    struct APITarget {
+        static constexpr std::string_view LOAD_SETTINGS = "/api/settings/load"sv;
+        static constexpr std::string_view SAVE_SETTINGS = "/api/settings/save"sv;
+        static constexpr std::string_view MAX_FILESIZE = "/api/downloader/settings/max_file_size"sv;
+        static constexpr std::string_view DOWNLOADS_FOLDER = "/api/downloader/settings/folder"sv;
+        static constexpr std::string_view DL_RESOURCE = "/api/downloader/settings/resource_and_prefix"sv;
+        static constexpr std::string_view WHITELIST = "/api/white_list/users"sv;
+        static constexpr std::string_view BLACKLIST = "/api/black_list/users"sv;
+        static constexpr std::string_view ROLE_FILTER_LVL = "/api/validator/settings/role_filter_level"sv;
+        static constexpr std::string_view WHITELIST_ONLY = "/api/validator/settings/whitelist_only"sv;
+        static constexpr std::string_view RECONNECT_TIMEOUT = "/api/irc_client/settings/reconnect_timeout"sv;
+        static constexpr std::string_view JOIN_CHANNEL = "/api/irc_client/join"sv;
+        static constexpr std::string_view PART_CHANNEL = "/api/irc_client/part"sv;
+        static constexpr std::string_view LAST_MESSAGES = "/api/vidget/chat/last_messages"sv;
+        static constexpr std::string_view SHOW_CHAT = "/api/vidget/chat/show"sv;
+    };
+
+    struct Settings {
+        static constexpr std::string_view ROLE_FILTER_LEVEL = "RoleFilterLevel"sv;
+        static constexpr std::string_view FILE_SIZE = "FileSize"sv;
+        static constexpr std::string_view PATH = "Path"sv;
+        static constexpr std::string_view USER_NAME = "UserName"sv;
+        static constexpr std::string_view CHANNEL = "Channel"sv;
+        static constexpr std::string_view Resource = "Resource"sv;
+        static constexpr std::string_view PREFIX = "Prefix"sv;
+        static constexpr std::string_view RECONNECT_TIMEOUT = "ReconnectTimeout"sv;
+        static constexpr std::string_view ENABLED = "Enabled"sv;
+        static constexpr std::string_view PATH = "Path"sv;
+        static constexpr std::string_view PATH = "Path"sv;
+        static constexpr std::string_view PATH = "Path"sv;
+        static constexpr std::string_view PATH = "Path"sv;
+
+
+    };
+}
