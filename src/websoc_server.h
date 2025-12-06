e392cadf044a6dda4220ec83e0c35375deb1a08e
myanwc <anton.kostyrev@yandex.ru>
2025-12-06 15:50:53 +0300

    Initial

---
+++ src/websoc_server.h
@@ -0,0 +1,12 @@
+#include <boost/beast.hpp>
+#include <boost/asio.hpp>
+
+namespace websoc_server {
+
+    namespace asio = boost::asio;
+    namespace beast = boost::beast;
+    namespace ws = beast::websocket;
+
+
+
+}
