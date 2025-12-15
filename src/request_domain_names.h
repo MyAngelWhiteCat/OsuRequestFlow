8d55d851b44e1ef0c093f35bd2d0da80d3f5ae8f
myanwc <anton.kostyrev@yandex.ru>
2025-12-15 13:13:15 +0300

    Add new names

--- src/request_domain_names.h
+++ src/request_domain_names.h
@@ -11,7 +11,10 @@ namespace gui_http {
         static constexpr std::string_view LOAD_SETTINGS = "/api/settings/load"sv;
         static constexpr std::string_view SAVE_SETTINGS = "/api/settings/save"sv;
         static constexpr std::string_view MAX_FILESIZE = "/api/downloader/settings/max_file_size"sv;
+        static constexpr std::string_view MESURE_SPEED = "/api/downloader/mesure_speed"sv;
+        static constexpr std::string_view DL_SERVER_STATUS = "/api/downloader/dl_server_status"sv;
         static constexpr std::string_view DOWNLOADS_FOLDER = "/api/downloader/settings/folder"sv;
+        static constexpr std::string_view BASE_SERVERS = "/api/downloader/base_servers"sv;
         static constexpr std::string_view DL_RESOURCE = "/api/downloader/settings/resource_and_prefix"sv;
         static constexpr std::string_view WHITELIST = "/api/white_list/users"sv;
         static constexpr std::string_view BLACKLIST = "/api/black_list/users"sv;
@@ -20,8 +23,8 @@ namespace gui_http {
         static constexpr std::string_view RECONNECT_TIMEOUT = "/api/irc_client/settings/reconnect_timeout"sv;
         static constexpr std::string_view JOIN_CHANNEL = "/api/irc_client/join"sv;
         static constexpr std::string_view PART_CHANNEL = "/api/irc_client/part"sv;
-        static constexpr std::string_view LAST_MESSAGES = "/api/vidget/chat/last_messages"sv;
-        static constexpr std::string_view SHOW_CHAT = "/api/vidget/chat/show"sv;
+        static constexpr std::string_view LAST_MESSAGES = "/api/widget/chat/last_messages"sv;
+        static constexpr std::string_view SHOW_CHAT = "/api/widget/chat/show"sv;
     };
 
     struct Settings {
@@ -34,7 +37,8 @@ namespace gui_http {
         static constexpr std::string_view PREFIX = "Prefix"sv;
         static constexpr std::string_view RECONNECT_TIMEOUT = "ReconnectTimeout"sv;
         static constexpr std::string_view ENABLED = "Enabled"sv;
-
-
+        static constexpr std::string_view IS_MESURED = "IsMesured"sv;
+        static constexpr std::string_view IS_ANY_AVAILABLE = "IsAnyAvailable"sv;
+        static constexpr std::string_view STATUS = "Status"sv;
     };
 }
